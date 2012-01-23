7863b6feaf84fb38ef5a8d504528f65ca298221b
Petko Minkov <pminkov@gmail.com>
2012-01-22 17:24:17 -0800

    Implement BFS.

--- graph.scala
+++ graph.scala
@@ -1,9 +1,13 @@
+import scala.collection.mutable.Queue
+
 class Graph(n: Int) {
   var edges = new Array[List[Int]](n)
 
   for (i <- 0 until n) {
     edges(i) = List[Int]()
   }
+
+  def nodesCount = n
 
   def addEdge(from: Int, to: Int) = {
     edges(from) = to :: edges(from)
@@ -20,6 +24,49 @@ class Graph(n: Int) {
   }
 }
 
+def BFS(g: Graph, from: Int, to: Int) = {
+  var queue = new Queue[Int]()
+  var visited = new Array[Boolean](g.nodesCount)
+  var prev = new Array[Int](g.nodesCount)
+
+  queue += from
+  visited(from) = true
+  prev(from) = -1
+
+  var found = false
+  while (queue.length > 0 && !found) {
+    var node = queue.dequeue
+
+    if (node != to) {
+      g.edges(node).foreach((to: Int) => {
+        if (!visited(to)) {
+          queue.enqueue(to)
+  	  visited(to) = true
+	  prev(to) = node
+        }
+      })
+    } else {
+      found = true
+    }
+  }
+
+  var maybeNot = ""
+  if (!found) maybeNot = "not "
+  println("A path from " + from + " to " + to + " was " + maybeNot + "found.")
+
+  if (found) {
+    var path = List[Int]()
+    var currentNode = to
+    while (currentNode != -1) {
+      path = currentNode :: path 
+      currentNode = prev(currentNode)
+    }
+    path.foreach((node: Int) => {
+      print(" " + node)
+    })
+    println
+  }
+}
 
 var g = new Graph(5)
 g.addEdge(0, 1)
@@ -28,16 +75,5 @@ g.addEdge(2, 3)
 g.addEdge(0, 3)
 g.addEdge(3, 4)
 
-//g.printGraph()
-
-val q = new Queue[Int]()
-
-/*
-
-var a = List[Int]()
-a = 5 :: a
-a = 6 :: a
-println(a.length)
-//var g = new Graph(5)
-
-*/
+BFS(g, 0, 4)
+BFS(g, 4, 0)
